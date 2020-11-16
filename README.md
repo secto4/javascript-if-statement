4ffc975f406fddba7a4141dbdff8421f44d52f42
Pal Daniel <daniel.pal@robot-x.hu>
2020-11-16 07:54:54 +0100

    addition

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# javascript-if-statement
-Native javascript if statement
+### Native javascript if statement
+
+Example:
+```javascript
+var el = document.getElementById('label');
+var lst = {
+  name: true
+};
+
+el.innerHTML = IfStatement(el.innerHTML, lst);
+```
--- main.js
+++ main.js
@@ -1,15 +1,24 @@
-function IfStatement (s, a) {
+/*
+* If statement function
+*
+* {!String} s Text content
+* {!Object} o List of variables
+*/
+function IfStatement (s, o) {
   if (s && (typeof s === 'string') && s.length) {
     s = s.replace(/{if\s(.+?)}(.+?){\/if}/g, function (match, variable, content) {
-      return ((typeof a[variable] !== "undefined") && a[variable]) ? content : "";
+      return ((typeof o[variable] !== 'undefined') && o[variable]) ? content : '';
     });
   }
 
   return s;
 }
 
+/*
+* Main function
+*/
 function main () {
-  var el = document.getElementById("label");
+  var el = document.getElementById('label');
   var lst = {
     name: true
   };
